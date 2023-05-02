6c08a8165e79050752385cc0d011ce7c55e4d265
Otaviano <otavianosilva.escritor@gmail.com>
2023-05-02 17:00:10 -0300

    inicio da view

--- view.py
+++ view.py
@@ -1,9 +1,25 @@
 from tkinter import *
 
 class View(Tk):
+
+    PAD = 10
+
     def __init__(self, controller) -> None:
         super().__init__()
         self.controller = controller
 
+        self.title('PyCalc1')
+
+        self._make_main_frame()
+        self._make_entry()
+
     def main(self):
         self.mainloop()
+
+    def _make_main_frame(self):
+        self.main_frame = Frame(self)
+        self.main_frame.pack(padx=self.PAD, pady=self.PAD)
+
+    def _make_entry(self):
+        entry = Entry(self.main_frame)
+        entry.pack()
